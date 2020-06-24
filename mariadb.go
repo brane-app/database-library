2fa9aa0be710fcb8980c67d3af6c690999739372
Noah Harris <dakoolstwunn@gmail.com>
2020-06-24 00:10:04 -0400

    Create tables
    Create a map of table name -> structure to be created. If they are not
    created at startup, create them

--- mariadb.go
+++ mariadb.go
@@ -1,13 +1,45 @@
 package monkebase
 
 import (
-	_ "github.com/go-sql-driver/mysql"
-
 	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
 	database *sql.DB
+	tables   map[string]string = map[string]string{
+		"content": `
+			id CHAR(36) UNIQUE PRIMARY KEY NOT NULL,
+			file_url VARCHAR(63) NOT NULL,
+			author VARCHAR(63) NOT NULL,
+			mime VARCHAR(63) NOT NULL,
+			like_count BIGINT UNSIGNED NOT NULL,
+			dislike_count BIGINT UNSIGNED NOT NULL,
+			repub_count BIGINT UNSIGNED NOT NULL,
+			view_count BIGINT UNSIGNED NOT NULL,
+			comment_count BIGINT UNSIGNED NOT NULL,
+			created BIGINT UNSIGNED NOT NULL,
+			featured BOOLEAN,
+			featurable BOOLEAN,
+			removed BOOLEAN,
+			nsfw BOOLEAN`,
+		"users": `
+			id CHAR(36) UNIQUE PRIMARY KEY NOT NULL,
+			nick VARCHAR(63) NOT NULL,
+			bio VARCHAR(255),
+			subscriber_count BIGINT UNSIGNED NOT NULL,
+			subscription_count BIGINT UNSIGNED NOT NULL,
+			created BIGINT UNSIGNED NOT NULL`,
+		"tags": `
+			id CHAR(36) NOT NULL,
+			tag CHAR(63) NOT NULL,
+			CONSTRAINT no_dupe_tags UNIQUE(id, tag)`,
+		"auth": `
+			id CHAR(36) UNIQUE PRIMARY KEY NOT NULL,
+			hash BINARY(60)`,
+	}
 )
 
 const (
@@ -23,5 +55,17 @@ func connect(address string) {
 
 	if err = database.Ping(); err != nil {
 		panic(err)
+	}
+
+	create()
+}
+
+func create() {
+	var err error
+	var table, structure string
+	for table, structure = range tables {
+		if _, err = database.Query(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (%s)", table, structure)); err != nil {
+			panic(err)
+		}
 	}
 }
