77c2da71e082a3e752823d81dc065eacf058713a
Noah Harris <dakoolstwunn@gmail.com>
2020-06-25 17:44:13 -0400

    Implement content writing
    Implement the code for writing and updating content

--- content.go
+++ content.go
@@ -1,7 +1,18 @@
 package monkebase
 
-func WriteContent(content interface{}) (err error) {
-	_, err = database.Query("")
+func WriteContent(content Mappable) (err error) {
+	var mapped map[string]interface{} = content.Map()
+	if err = setTags(mapped["id"].(string), mapped["tags"].([]string)); err != nil {
+		return
+	}
+
+	delete(mapped, "tags")
+
+	var query string
+	var values []interface{}
+	query, values = makeSQLInsertable(CONTENT_TABLE, mapped)
+
+	_, err = database.Query(query, values...)
 	return
 }
 
@@ -14,5 +25,29 @@ func ReadManyContent(index, limit int) (content []interface{}, size int, err err
 }
 
 func ReadAuthorContent(ID string, index, limit int) (content []interface{}, size, err error) {
+	return
+}
+
+func setTags(ID string, tags []string) (err error) {
+	var p_string = manyParamString(len(tags))
+	var statement string = "DELETE FROM " + TAG_TABLE + " WHERE id=? AND tag NOT IN (" + p_string + ")"
+	var faces []interface{} = append(
+		[]interface{}{ID},
+		interfaceStrings(tags...)...,
+	)
+
+	if _, err = database.Query(statement, faces...); err != nil {
+		return
+	}
+
+	statement = "REPLACE INTO " + TAG_TABLE + " (id, tag) VALUES (?, ?)"
+
+	var tag interface{}
+	for _, tag = range tags {
+		if _, err = database.Query(statement, ID, tag); err != nil {
+			break
+		}
+	}
+
 	return
 }
